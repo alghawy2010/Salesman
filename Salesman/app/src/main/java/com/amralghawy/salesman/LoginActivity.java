6259f8f902e386aeed280351a14ab34cd63955a4
alghawy2010 <alghawy2010@gmail.com>
2017-05-02 23:48:18 +0300

    Current Location Fix

--- Salesman/app/src/main/java/com/amralghawy/salesman/LoginActivity.java
+++ Salesman/app/src/main/java/com/amralghawy/salesman/LoginActivity.java
@@ -10,24 +10,33 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity implements LocationListener{
 
+    private final String TAG = LoginActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate - Start");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Log.d(TAG, "onCreate - End");
     }
 
     // Create onClick event for Sign-in button
     public void signInBttnOnClick(View view) {
+        Log.d(TAG, "signInBttnOnClick - Start");
         // 1- Get current User location
         // Check if GPS is granted or not
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             // Permission is granted now
+            Log.d(TAG, "signInBttnOnClick - Permission is granted");
             // Get current user location
 
             //ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, );
@@ -41,37 +50,85 @@ public class LoginActivity extends AppCompatActivity implements LocationListener
 
             Location currentLocation = null;
             if (isGPSEnabled) {
+                Log.d(TAG, "signInBttnOnClick - GPS Enabled");
                 // Get current location from GPS
-                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+                currentLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                //locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, this);
+
+                if (currentLocation != null) {
+                    Log.d(TAG, "signInBttnOnClick - Call onLocationChanged method");
+                    onLocationChanged(currentLocation);
+                }
+                else {
+                    Toast.makeText(getBaseContext(), "No Location Provider Found Check Your Code", Toast.LENGTH_SHORT).show();
+                }
             }
             else if (isNetworkEnabled) {
+                Log.d(TAG, "signInBttnOnClick - Network Enabled");
                 // Get current location from Network
                 currentLocation = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
             }
             else {
                 // Error
+                Log.d(TAG, "signInBttnOnClick - GPS and Network are not enabled");
+                return;
             }
 
-            //EditText text = (EditText) this.findViewById(R.id.editText2);
-            //text.setText("Lat: "+ currentLocation.getLatitude() + "Long: "+ currentLocation.getLongitude());
+            // Get User name and Password
+            EditText userName = (EditText) this.findViewById(R.id.userNameEditText);
+            EditText password = (EditText) this.findViewById(R.id.passwordEditText);
+
+            // 2- Prepare JSON request to cloud server
+            
+            // 3- Parse JSON response
+
+            // 4- Send response to next activity to display a list of nearby customers
         }
         else {
             // Location permission is required
+            Log.d(TAG, "signInBttnOnClick - Permission is not granted");
             // Show Alert to enable location service
+            Toast.makeText(getBaseContext(), "No Service Provider Available", Toast.LENGTH_SHORT).show();
+
+            // Ask for permissions
+            // ToDo: What shoud i do if user reject to grant below permission? is below permission enough or internet access also required
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 4);
+
+            /*
+            // Show dialog to enable network and GPS
+            final Context mContext = getBaseContext();
+            AlertDialog.Builder alertDialog = new AlertDialog.Builder(mContext);
+            alertDialog.setTitle("GPS Not Enabled");
+            alertDialog.setMessage("Do you wants to turn On GPS");
+
+
+            alertDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                    mContext.startActivity(intent);
+                }
+            });
+
+
+            alertDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.cancel();
+                }
+            });
+             */
         }
 
-        // 2- Prepare JSON request to cloud server
-
-        // 3- Parse JSON response
-
-        // 4- Send response to next activity to display a list of nearby customers
-
+        Log.d(TAG, "signInBttnOnClick - End");
     }
 
     @Override
     public void onLocationChanged(Location location) {
-        EditText text = (EditText) this.findViewById(R.id.editText2);
+        Log.d(TAG, "onLocationChanged - Start");
+
+        EditText text = (EditText) this.findViewById(R.id.userNameEditText);
         text.setText("Lat: "+ location.getLatitude() + "Long: "+ location.getLongitude());
+
+        Log.d(TAG, "onLocationChanged - End");
     }
 
     @Override
